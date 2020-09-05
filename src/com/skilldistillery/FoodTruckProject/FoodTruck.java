ca84b27b44dd20f43862483b09d3cc228e6c83a8
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-09-04 20:33:27 -0600

    Created vairables and toString

--- src/com/skilldistillery/FoodTruckProject/FoodTruck.java
+++ src/com/skilldistillery/FoodTruckProject/FoodTruck.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.FoodTruckProject;
 
 public class FoodTruck {
+	private String name;
+	private String category;
+	private int rating;
+	private int assignID = 0;
+	private int uniqueID;
+	
+	
+	public FoodTruck(String truckName, String menuType, int userRating) {
+		name = truckName;
+		category = menuType;
+		rating = userRating;
+		++assignID;
+		uniqueID = assignID;
+		
+	}
+	@Override
+	public String toString() {
+		return "FoodTruck " + uniqueID + ":  " + name + "\tServes:  " + category + "\tYou rated " + rating + " out of 10";
+	}
+	
+
 
 }
